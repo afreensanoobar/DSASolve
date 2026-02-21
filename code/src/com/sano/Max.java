e1abd179b38091af32e26e4b2d566ecfd77a0c51
afreensanoobar <afreensanoobar0026@gmail.com>
2026-02-21 15:44:32 +0530

    P

--- code/src/com/sano/Max.java
+++ code/src/com/sano/Max.java
@@ -2,16 +2,3 @@ package com.sano;
 
 public class Max {
     public static void main(String[] args) {
-        int[] arr = {1,3,45,67,8};
-        System.out.println(max(arr));
-    }
-    static int maxRange(int[] arr , int start , int end){
-        int maxVal= arr[0];
-        for (int i=1;i<arr.length;i++ ){
-            if(arr[i]>maxVal){
-                maxVal=arr[i];
-            }
-        }
-        return maxVal;
-    }
-}
